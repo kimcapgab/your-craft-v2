f0972aed4731c067eac52a4413ea9d3b36a949d4
Kim Capilla <kcapilla@Kims-MBP.attlocal.net>
2022-02-09 17:37:52 -0500

    minor changes to the backend

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 let MONGODB_URI =
-  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/yourCraftDB";
+  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/yourCraftv2DB";
 
 // this is for model.findByIdAndUpdate() method,
 // specifically so that {new: true} is the default.
