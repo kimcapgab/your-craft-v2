6d9252ab83c47341db3b40aed1f2df7c0f7f5060
Kim Capilla <kcapilla@Kims-MBP.attlocal.net>
2022-02-10 14:35:11 -0500

    before deploying

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home/Home'
+import SignUp from './screens/SignUp/SignUp'
+import SignIn from './screens/SignIn/SignIn'
+import EditForm from './screens/EditForm/EditForm'
+import BevGallery from './screens/BevGallery/BevGallery'
+import BevID from './screens/BevID/BevID'
+import CreateForm from './screens/CreateForm/CreateForm'
+import { useEffect, useState } from 'react';
+import {verifyUser} from './services/userApi'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout'
+import About from './screens/About/About';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+
 
 function App() {
+
+  const [user, setUser] = useState(null)
+  const [toggle, setToggle] = useState(false)
+
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser()
+      user ? setUser(user) : setUser(null)
+    }
+    fetchUser()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout user={user} setUser={setUser} >
+      <Routes>
+        <Route path='/' element={<Home user={user} setUser={setUser} toggle={toggle} setToggle={setToggle}/>} />
+        <Route path="/create"
+          element={<CreateForm user={user} setToggle={setToggle}/>} />
+        <Route path='/sign-up' element={<SignUp user={user} setUser={setUser}/>} />
+        <Route path='/sign-in' element={<SignIn user={user} setUser={setUser} />} />
+        <Route path='/edit/:id' element={<EditForm user={user} setToggle={setToggle}/>} />
+          <Route path='/bev/:id' element={<BevID user={user} toggle={toggle} />} />
+          <Route path='/bevs/:typeOf' element={<BevGallery user={user} toggle={toggle} setToggle={setToggle} />} />
+        <Route path='/aboutUs' element={<About/>}/>
+      </Routes>
+      </Layout>
+    
     </div>
   );
 }
